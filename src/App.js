7644a5d03ccd040d13b9bbb90e4afa3856ce2775
Laura Löschmann <44237401+LauraLoe@users.noreply.github.com>
2023-02-13 11:38:10 +0100

    all tests passed

--- src/App.js
+++ src/App.js
@@ -1,57 +1,52 @@
 import './App.css';
-import React, {useState, useEffect, useRef} from 'react'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleArrowDown } from "@fortawesome/free-solid-svg-icons";
-import { faCircleArrowUp } from "@fortawesome/free-solid-svg-icons";
-import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
-import { faCircleStop } from "@fortawesome/free-solid-svg-icons";
-import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
+import React from 'react'
 
-function App() {
-  const [breakLength, setBreakLength] = useState(5*60);
-  const [sessionLength, setSessionLength] = useState(25*60);
-  const [time, setTime] = useState(sessionLength);
-  const [timerOn, setTimerOn] = useState(false);
-  const [breakTime, setBreakTime] = useState(false);
-  const previousTime = useRef(null);
+function App(){
+  const [breakLength, setBreakLength] = React.useState(5);
+  const [sessionLength, setSessionLength] = React.useState(25);
+  const [time, setTime] = React.useState(25*60);
+  const [timerOn, setTimerOn] = React.useState(false);
+  const [breakTime, setBreakTime] = React.useState(false);
+  const previousTime = React.useRef(null);
+  const previousSessionLength = React.useRef(null);
 
-  useEffect(() => {
+  React.useEffect(() => {
     previousTime.current = time;
   }, [time]);
 
-  const formatLength = (length) => {
-    return Math.floor(length / 60);
-  }
+  React.useEffect(() => {
+    previousSessionLength.current = sessionLength;
+  }, [sessionLength]);
 
-//   // const sleep = (milliseconds) => {
-//   //   console.log("sleeping")
-//   //   return new Promise(resolve => setTimeout(resolve, milliseconds))
-//   // }
+  const playAlarm = () => {
+    document.getElementById("beep").play();
+  }
 
   const decrement = (ref = '') => {
     if (ref === 'break') {
-      if (breakLength - 60 > 0) {
-        setBreakLength((prev) => prev - 60);
+      if (breakLength - 1 > 0) {
+        setBreakLength((prev) => prev - 1);
       }
     }
     else {
-      if (sessionLength - 60 > 0) {
-        setSessionLength((prev) => prev - 60);
-        setTime(sessionLength - 60);
+      if (previousSessionLength.current - 1 > 0) {
+        previousSessionLength.current -= 1;
+        setSessionLength(previousSessionLength.current);
+        setTime(previousSessionLength.current*60);
       }
     }
   }
 
   const increment = (ref = '') => {
     if (ref === 'break') {
-      if (breakLength + 60 <= 60*60) {
-      setBreakLength((prev) => prev + 60);
+      if (breakLength + 1 <= 60) {
+      setBreakLength((prev) => prev + 1);
       }
     }
     else {
-      if (sessionLength + 60 <= 60*60) {
-      setSessionLength((prev) => prev + 60);
-      setTime(sessionLength + 60);
+      if (sessionLength + 1 <= 60) {
+      setSessionLength((prev) => prev + 1);
+      setTime((sessionLength + 1)*60);
       }
     }
   }
@@ -66,56 +61,53 @@ function App() {
     )
   }
 
-  const playAlarm = () => {
-    document.getElementById("beep").play();
+  const controlTime = () => {
+    let second = 1000;
+    let nextTime = new Date().getTime() + second;
+    let breaking = breakTime
+    if (!timerOn) {
+      let interval = setInterval(() => {
+        let currentTime = new Date().getTime();
+        if (currentTime > nextTime) {
+          if (previousTime.current <= 0 && breaking === false) {
+            setBreakTime(true);
+            breaking = true;
+            console.log('Break!');
+            setTime((prev) => (breakLength*60));
+          }
+          else if (previousTime.current <= 0 && breaking === true) {
+            setBreakTime(false);
+            breaking = false;
+            console.log('Session!');
+            setTime((prev) => (sessionLength*60));
+          }
+          else if (previousTime.current <= 1) {
+            playAlarm();
+            setTime((prev) => prev - 1);
+          }
+          else {
+            setTime((prev) => prev - 1);
+          }
+          nextTime += second;
+        }
+      }, 10);
+      localStorage.clear();
+      localStorage.setItem("interval-id", interval)
+    }
+
+    if (timerOn) {
+      clearInterval(localStorage.getItem("interval-id"))
+    }
+    setTimerOn(!timerOn)
   }
 
-const timer = () => {
-  let second = 1000;
-  let next = new Date().getTime() + second;
-  let breaking = breakTime
-  if(!timerOn) {
-    let interval = setInterval(() => {
-      let current = new Date().getTime();
-      if (current > next) {
-        if (previousTime.current <= 0 && breaking === false) {
-          setBreakTime(true);
-          breaking = true;
-          console.log('break!');
-          setTime((prev) => breakLength);
-        }
-        else if (previousTime.current <= 0 && breaking === true) {
-          setBreakTime(false);
-          breaking = false;
-          console.log('session!');
-          setTime((prev) => sessionLength);
-        }
-        else if (previousTime.current <= 1) {
-          playAlarm();
-          setTime((prev) => prev - 1);
-        }
-        else {
-          setTime((prev) => prev - 1);
-        }
-        next += second;
-      }
-    }, 1);
-    localStorage.clear();
-    localStorage.setItem('interval-id', interval);
-  }
-  if (timerOn) {
-    clearInterval(localStorage.getItem('interval-id'))
-  }
-  setTimerOn(!timerOn);
-}
-
-  const reset = () => {
+  const resetTime = () => {
     document.getElementById("beep").pause();
     document.getElementById("beep").currentTime = 0;
-    setBreakLength(5*60);
-    setSessionLength(25*60);
-    setTime(25*60);
     setBreakTime(false);
+    setBreakLength(5);
+    setSessionLength(25);
+    setTime(25*60);
     clearInterval(localStorage.getItem("interval-id"))
     setTimerOn(false);
   }
@@ -126,16 +118,16 @@ const timer = () => {
       <div className="control">
         <div id="break-label">Break Length
           <div className="wrapper">
-            <div id="break-decrement" onClick={() => decrement('break')}><FontAwesomeIcon icon={faCircleArrowDown} /></div>
-            <div id="break-length">{formatLength(breakLength)}</div>
-            <div id="break-increment" onClick={() => increment('break')}><FontAwesomeIcon icon={faCircleArrowUp} /></div>
+            <button className="btn btn-light" onClick={() => decrement('break')} id="break-decrement">-</button>
+            <div id="break-length">{breakLength}</div>
+            <button className="btn btn-light" onClick={() => increment('break')} id="break-increment">+</button>
           </div>
         </div>
         <div id="session-label">Session Length
           <div className="wrapper">
-            <div id="session-decrement" onClick={decrement}><FontAwesomeIcon icon={faCircleArrowDown} /></div>
-            <div id="session-length">{formatLength(sessionLength)}</div>
-            <div id="session-increment" onClick={increment}><FontAwesomeIcon icon={faCircleArrowUp} /></div>
+          <button className="btn btn-light" onClick={decrement} id="session-decrement">-</button>
+            <div id="session-length">{sessionLength}</div>
+            <button className="btn btn-light" onClick={increment} id="session-increment">+</button>
           </div>
         </div>
       </div>
@@ -143,10 +135,10 @@ const timer = () => {
         <div id="time-left">{displayTime()}</div>
       </div>
       <div id="regulator">
-        <div id="start_stop" onClick={timer}>
-          {timerOn ? <FontAwesomeIcon icon={faCircleStop}/> : <FontAwesomeIcon icon={faCirclePlay}/>}
-        </div>
-        <div id="reset"><FontAwesomeIcon icon={faArrowsRotate} onClick={reset}/></div>
+        <button id="start_stop" className="btn btn-light" onClick={controlTime}>
+          {(timerOn ? ("Stop") : ("Start"))}
+        </button>
+        <button id="reset" className="btn btn-warning" onClick={resetTime}>Reset</button>
       </div>
       <audio id="beep" src="http://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/music/start.ogg"/>
     </div>
